5bde5b6b3a049706e1c647c3c94d49ec247e734e
saraforte290 <saraforte290@gmail.com>
2025-01-23 10:40:12 +0100

    Update sql database create

--- create-tables.sql
+++ create-tables.sql
@@ -2,7 +2,7 @@ CREATE DATABASE `curso_node` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8
 
 -- curso_node.usuarios definition
 
-CREATE TABLE `curso_node`.`usuarios` (
+CREATE TABLE `usuarios` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
   `apellidos` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -10,14 +10,16 @@ CREATE TABLE `curso_node`.`usuarios` (
   `email` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
   `status` tinyint(1) NOT NULL,
   `fecha_registro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `password` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `rol` enum('Admin','Client') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
 -- curso_node.tareas definition
 
-CREATE TABLE `curso_node`.`tareas` (
+CREATE TABLE `tareas` (
   `id` int NOT NULL AUTO_INCREMENT,
   `titulo` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
   `prioridad` enum('urgent','monthly','daily') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'diaria',
@@ -25,5 +27,5 @@ CREATE TABLE `curso_node`.`tareas` (
   `usuario_id` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `usuariotarea_fk` (`usuario_id`),
-  CONSTRAINT `usuariotarea_fk` FOREIGN KEY (`usuario_id`) REFERENCES `curso_node`.`usuarios` (`id`)
+  CONSTRAINT `usuariotarea_fk` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
